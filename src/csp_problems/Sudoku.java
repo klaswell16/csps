29047fe60f4894893dbc9fe0737aede3be0f91ef
Kayan Laswell <kcclaswell@gmail.com>
2025-04-03 17:07:03 -0400

    test cases other than 5 are solved

--- src/csp_problems/Sudoku.java
+++ src/csp_problems/Sudoku.java
@@ -3,104 +3,76 @@ package csp_problems;
 import java.io.*;
 import java.util.*;
 
-public class Sudoku implements CSPProblem<Square,Integer> {
-
-    private final Map<Square, Variable<Square,Integer>> allVariables;
-
-    //Map every square to the set of its neighboring squares.
-    //"Neighbors" are those in the same row, column, or 3x3 box
-    private final Map<Square,Set<Square>> neighbors = new HashMap<>();
-
-    //Name of the file that contains the test case.
+public class Sudoku implements CSPProblem<Square, Integer> {
+    private final Map<Square, Variable<Square, Integer>> allVariables;
+    private final Map<Square, Set<Square>> neighbors = new HashMap<>();
     private final String filename;
 
     public Sudoku(String filename) {
         this.filename = filename;
         allVariables = getAllVariables();
-        //for each row, get all the squares in that row.
-        for (int i=0; i<9; i++) {
-            //build the row neighbor set (all square in the same row)
+
+        for (int i = 0; i < 9; i++) {
             Set<Square> rowNeighbors = new HashSet<>();
-            for (int j=0; j<9; j++) {
-                Square sq = new Square(i,j);
-                rowNeighbors.add(sq);
+            for (int j = 0; j < 9; j++) {
+                rowNeighbors.add(new Square(i, j));
             }
-            //associate this neighbor set with each square in that row
-            for(int j=0; j<9; j++){
-                Square sq = new Square(i,j);
-                neighbors.put(sq, new HashSet<>(rowNeighbors));
+            for (int j = 0; j < 9; j++) {
+                neighbors.put(new Square(i, j), new HashSet<>(rowNeighbors));
             }
         }
-        //for each column, get all squares in that column
-        for (int j=0; j<9; j++) {
-            //build the column neighbor set (all squares in the same column)
+
+        for (int j = 0; j < 9; j++) {
             Set<Square> columnNeighbors = new HashSet<>();
-            for (int i=0; i<9; i++) {
-                Square sq = new Square(i,j);
-                columnNeighbors.add(sq);
+            for (int i = 0; i < 9; i++) {
+                columnNeighbors.add(new Square(i, j));
             }
-            //associate this neighbor set with each square in that column
-            for (int i=0; i<9; i++) {
-                Square sq = new Square(i,j);
-                neighbors.get(sq).addAll(columnNeighbors);
+            for (int i = 0; i < 9; i++) {
+                neighbors.get(new Square(i, j)).addAll(columnNeighbors);
             }
         }
-        for(int i=0; i<3; i++) {
-            for (int j=0; j<3; j++) {
-                //build the box neighbor set (all squares in the same 3x3 box)
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
                 Set<Square> boxNeighbors = new HashSet<>();
-                for (int x=0; x<3; x++) {
-                    for (int y=0; y<3; y++) {
-                        Square sq = new Square (i*3+x, j*3+y);
-                        boxNeighbors.add(sq);
+                for (int x = 0; x < 3; x++) {
+                    for (int y = 0; y < 3; y++) {
+                        boxNeighbors.add(new Square(i * 3 + x, j * 3 + y));
                     }
                 }
-                //associate this neighbor set with each square in that 3x3 box
-                for (int x=0; x<3; x++) {
-                    for (int y=0; y<3; y++) {
-                        Square sq = new Square (i*3+x, j*3+y);
-                        neighbors.get(sq).addAll(boxNeighbors);
+                for (int x = 0; x < 3; x++) {
+                    for (int y = 0; y < 3; y++) {
+                        neighbors.get(new Square(i * 3 + x, j * 3 + y)).addAll(boxNeighbors);
                     }
                 }
             }
         }
-        //remove a square from its own neighbor set.
-        for(Map.Entry<Square,Set<Square>> e : neighbors.entrySet()){
+
+        for (Map.Entry<Square, Set<Square>> e : neighbors.entrySet()) {
             e.getValue().remove(e.getKey());
         }
     }
 
-    public Map<Square,Variable<Square,Integer>> getAllVariables() {
-        Map<Square,Variable<Square,Integer>> allVariables = new HashMap<>();
-        try {
-            BufferedReader in = new BufferedReader(new FileReader(filename));
+    public Map<Square, Variable<Square, Integer>> getAllVariables() {
+        Map<Square, Variable<Square, Integer>> allVariables = new HashMap<>();
+        try (BufferedReader in = new BufferedReader(new FileReader(filename))) {
             String line;
-            List<Integer> defaultDomain = List.of(1,2,3,4,5,6,7,8,9);
-            //i: row number, j: column number
-            for (int i=0; i<9; i++) {
+            List<Integer> defaultDomain = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+            for (int i = 0; i < 9; i++) {
                 if ((line = in.readLine()) != null && !line.isEmpty()) {
                     String[] numbers = line.trim().split(" ");
-                    for (int j=0; j<9; j++) {
-                        Square vName = new Square(i,j);
+                    for (int j = 0; j < 9; j++) {
+                        Square vName = new Square(i, j);
                         int number = Integer.parseInt(numbers[j]);
-                        Variable<Square, Integer> v;
-                        if (number>0 && number<10) {
-                            //this square is pre-assigned
-                            v = new Variable<>(vName, new LinkedList<>(List.of(number)));
-                        } else {
-                            //this square is open
-                            v = new Variable<>(vName, new LinkedList<>(defaultDomain));
-                        }
-                        allVariables.put(vName,v);
+                        Variable<Square, Integer> v = (number > 0 && number < 10)
+                                ? new Variable<>(vName, new LinkedList<>(List.of(number)))
+                                : new Variable<>(vName, new LinkedList<>(defaultDomain));
+                        allVariables.put(vName, v);
                     }
                 } else {
-                    //if a line is empty, or if the remaining lines are missing
-                    //treat an empty or missing line as all squares in that line are open
-                    for (int j=0; j<9; j++) {
+                    for (int j = 0; j < 9; j++) {
                         Square vName = new Square(i, j);
-                        Variable<Square,Integer> v =
-                                new Variable<>(vName, new LinkedList<>(defaultDomain));
-                        allVariables.put(vName,v);
+                        allVariables.put(vName, new Variable<>(vName, new LinkedList<>(defaultDomain)));
                     }
                 }
             }
@@ -110,33 +82,30 @@ public class Sudoku implements CSPProblem<Square,Integer> {
         return allVariables;
     }
 
-    public void printPuzzle(Map<Square,Variable<Square,Integer>> allVariables) {
-        for (int i=0; i<9; i++) {
-            for (int j=0; j<9; j++) {
-                if (allVariables.get(new Square(i,j)).domain().size() > 1) {
+    public void printPuzzle(Map<Square, Variable<Square, Integer>> allVariables) {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (allVariables.get(new Square(i, j)).domain().size() > 1) {
                     System.out.print("[ ]");
                 } else {
-                    System.out.print("["+allVariables.get(new Square(i,j)).domain().getFirst()+"]");
+                    System.out.print("[" + allVariables.get(new Square(i, j)).domain().getFirst() + "]");
                 }
             }
             System.out.print("\r\n");
         }
     }
 
-    /**
-     * Given a square, return the neighboring squares as a list.
-     * "Neighbors" are those squares in the same row, column, or 3x3 box
-     * @param sq the square
-     * @return List of neighboring squares
-     */
-    public List<Square> getNeighborsOf(Square sq){
-        //TODO:
+    public List<Square> getNeighborsOf(Square sq) {
+        return new ArrayList<>(neighbors.get(sq));
     }
 
-    /**
-     * @return the list of pre-assigned squares
-     */
     public List<Square> getPreAssignedVariables() {
-        //TODO:
+        List<Square> preAssigned = new ArrayList<>();
+        for (Map.Entry<Square, Variable<Square, Integer>> entry : allVariables.entrySet()) {
+            if (entry.getValue().domain().size() == 1) {
+                preAssigned.add(entry.getKey());
+            }
+        }
+        return preAssigned;
     }
 }
--- src/csp_solutions/Backtracking_Sudoku.java
+++ src/csp_solutions/Backtracking_Sudoku.java
@@ -1,11 +1,10 @@
 package csp_solutions;
-
 import core_algorithms.BacktrackingSearch;
 import csp_problems.*;
-
+import csp_problems.CSPProblem.Variable;
+import java.util.*;
 
 public class Backtracking_Sudoku extends BacktrackingSearch<Square,Integer>{
-
     public Backtracking_Sudoku(Sudoku problem){
         super(problem);
     }
@@ -16,34 +15,56 @@ public class Backtracking_Sudoku extends BacktrackingSearch<Square,Integer>{
      * @return true if the tail has been revised (lost some values), false otherwise
      */
     public boolean revise(Square tail, Square head) {
-        //TODO:
-        //for each value in tail's domain, there must be a value in head's domain that's different
-        //if not, delete the value from the tail's domain
+        boolean revised = false;
+        Variable<Square, Integer> tailVar = getAllVariables().get(tail);
+        Variable<Square, Integer> headVar = getAllVariables().get(head);
 
+        Iterator<Integer> iterator = tailVar.domain().iterator();
+        while (iterator.hasNext()) {
+            int value = iterator.next();
+            if (headVar.domain().size() == 1 && headVar.domain().contains(value)) {
+                iterator.remove();
+                revised = true;
+            }
+        }
+        return revised;
     }
 
     /**
-     * Implementing the Minimum Remaining Values(MRV) ordering heuristic.
+     * Implementing the Minimum Remaining Values (MRV) ordering heuristic.
      * @return the variable with the smallest domain among all the unassigned variables;
-     *         null if all variables have been assigned
+     * null if all variables have been assigned
      */
-    public Square selectUnassigned(){
-       //TODO:
+    public Square selectUnassigned() {
+        Square minVar = null;
+        int minSize = Integer.MAX_VALUE;
+
+        for (Map.Entry<Square, Variable<Square, Integer>> entry : getAllVariables().entrySet()) {
+            Square var = entry.getKey();
+            Variable<Square, Integer> variable = entry.getValue();
+            if (!assigned(var) && variable.domain().size() < minSize) {
+                minSize = variable.domain().size();
+                minVar = var;
+            }
+        }
+        return minVar;
     }
 
     /**
      * @param args (no command-line argument is needed to run this program)
      */
     public static void main(String[] args) {
-        String filename = "./SudokuTestCases/TestCase1   .txt";
+        String filename = "./SudokuTestCases/TestCase10.txt";
         Sudoku problem = new Sudoku(filename);
         Backtracking_Sudoku agent = new Backtracking_Sudoku(problem);
-        System.out.println("loading puzzle from " + filename + "...");
+
+        System.out.println("Loading puzzle from " + filename + "...");
         problem.printPuzzle(problem.getAllVariables());
+
         if(agent.initAC3() && agent.search()){
             System.out.println("Solution found:");
             problem.printPuzzle(agent.getAllVariables());
-        }else{
+        } else {
             System.out.println("Unable to find a solution.");
         }
     }
